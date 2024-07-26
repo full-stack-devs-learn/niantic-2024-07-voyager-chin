0715c860c9adfb2ecc7fce5a1ba177595a77c12c
Chin Antalan <152952438+chinsomnia@users.noreply.github.com>
2024-07-26 13:38:51 -0700

    added 05-review file

---
+++ sprint-1/05-review/review_loops/src/main/java/com/example/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+
+    }
+    public static String[] createCCList()
+    {
+        String[] creditCardTypes = {"VISA", "MASTERCARD", "AMERICAN EXPRESS", "DISCOVER"};
+        return creditCardTypes;
+    }
+}
