f7b31843342ba46b8153ae7195b0a2952f130c38
Chin Antalan <152952438+chinsomnia@users.noreply.github.com>
2024-08-05 15:51:09 -0700

    Create Rectangle class

--- sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_2_objects/Rectangle.java
+++ sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_2_objects/Rectangle.java
@@ -2,4 +2,34 @@ package com.niantic.part_2_objects;
 
 public class Rectangle
 {
+    // Declare private variables for Rectangle:
+    private int width;
+    private int height;
+    private int area;
+
+    // Parameterless constructor:
+    public Rectangle()
+    {
+
+    }
+
+    // Parameterized constructor:
+    public Rectangle(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+
+    // Public 'getters' and 'setters':
+    public int getWidth() { return width; }
+
+    public void setWidth(int width) { this.width = width; }
+
+    public int getHeight() { return height; }
+
+    public void setHeight(int height) { this.height = height; }
+
+    public int getArea() { return this.width * this.height; }
+
+    public int calculateArea(int width, int height) { return width * height; }
 }
