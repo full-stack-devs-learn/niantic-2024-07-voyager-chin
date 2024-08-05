6f9e6015f1039f18e810ef39f5b73bdd66f89ceb
Chin Antalan <152952438+chinsomnia@users.noreply.github.com>
2024-08-05 13:53:10 -0700

    created Actor DAO class

--- sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_1_sakila_dtos/Actor.java
+++ sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_1_sakila_dtos/Actor.java
@@ -2,4 +2,33 @@ package com.niantic.part_1_sakila_dtos;
 
 public class Actor
 {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Actor()
+    {
+
+    }
+
+    public Actor(int id, String firstName, String lastName)
+    {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getActorId() { return id; }
+
+    public void setActorId(int id) { this.id = id; }
+
+    public String getFirstName() { return firstName; }
+
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getFullName() { return firstName + " " + lastName; }
 }
