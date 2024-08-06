78db98328ac1d75001a2baf7ba0584fca89e3f03
Chin Antalan <152952438+chinsomnia@users.noreply.github.com>
2024-08-05 22:59:49 -0700

    Created the Math Test class

--- sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_2_objects/MathTest.java
+++ sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_2_objects/MathTest.java
@@ -2,4 +2,56 @@ package com.niantic.part_2_objects;
 
 public class MathTest
 {
+    // Declare private variables for MathTest
+    private int score;
+    private int possiblePoint;
+    private String studentName;
+
+
+    // Parameterized constructor:
+    public MathTest(int possiblePoint, String studentName)
+    {
+        this.possiblePoint = possiblePoint;
+        this.studentName = studentName;
+    }
+
+
+    // Public 'getters' and 'setters':
+    public int getScore() { return score; }
+
+    public void setScore(int score)
+    {
+        this.score = score;
+    }
+
+    public int getPossiblePoints() { return possiblePoint; }
+
+    public String getStudentName() { return studentName; }
+
+    public int getPercent()
+    {
+        double percentage = (double) score / possiblePoint * 100;
+        return (int) percentage;
+    }
+
+    public String getLetterGrade()
+    {
+        if (getPercent() >= 90)
+        {
+            return "A";
+        }
+        else if (getPercent() >= 80) {
+            return "B";
+        }
+        else if (getPercent() >= 70) {
+            return "C";
+        }
+        else if (getPercent() >= 60) {
+            return "D";
+        }
+        else
+        {
+            return "F";
+        }
+    }
 }
