b9b018beac59ec57996cecf7cb9b5b9154c401d7
Chin Antalan <152952438+chinsomnia@users.noreply.github.com>
2024-08-08 23:28:13 -0700

    Working transaction report except by year and month

--- sprint-1/14-review/budget-tracker/src/main/java/com/niantic/application/BudgetTracker.java
+++ sprint-1/14-review/budget-tracker/src/main/java/com/niantic/application/BudgetTracker.java
@@ -1,10 +1,22 @@
 package com.niantic.application;
 
+import com.niantic.models.Transaction;
+import com.niantic.services.TransactionDao;
+
 import java.util.Scanner;
+
+//import static java.util.FormatProcessor.FMT;
+
+//import static java.lang.StringTemplate.STR;
 
 public class BudgetTracker
 {
     Scanner userInput = new Scanner(System.in);
+
+    TransactionDao transactionDao = new TransactionDao();
+    //Add more DAOs here
+
+    // <editor-fold desc="Home Page - BudgetTracker Application">
     public void run()
     {
 
@@ -17,7 +29,7 @@ public class BudgetTracker
                     System.out.println("add transaction");
                     break;
                 case 2:
-                    System.out.println("reports");
+                    displayReports();
                     break;
                 case 3:
                     System.out.println("add user");
@@ -65,5 +77,252 @@ public class BudgetTracker
         return Integer.parseInt(userInput.nextLine());
     }
 
+    // </editor-fold>
 
+    // <editor-fold desc="Reports">
+    private void displayReports()
+    {
+        while(true)
+        {
+            int choice = reportsSelection();
+            switch(choice)
+            {
+                case 1:
+                    transactionsByUser();
+                    break;
+                case 2:
+                    transactionsByMonth();
+                    break;
+                case 3:
+                    transactionsByYear();
+                    break;
+                case 4:
+                    transactionsBySubCategory();
+                    break;
+                case 5:
+                    transactionsByCategory();
+                    break;
+                case 0:
+                    // go back to home screen
+                    return;
+                default:
+                    System.out.println("That was an invalid selection, please select from the available options.");
+            }
+        }
+    }
+
+    private int reportsSelection()
+    {
+        System.out.println();
+        System.out.println("Reports");
+        System.out.println("--------------------------------------");
+        System.out.println("Select from the following options:");
+        System.out.println();
+        System.out.println("  1) Transactions By User");
+        System.out.println("  2) Transactions By Month");
+        System.out.println("  3) Transactions By Year");
+        System.out.println("  4) Transactions By Sub Category");
+        System.out.println("  5) Transactions By Category");
+        System.out.println("  0) Back");
+        System.out.println();
+
+        System.out.println("Enter your selection: ");
+        return Integer.parseInt(userInput.nextLine());
+    }
+
+    private void transactionsByUser()
+    {
+
+        System.out.println();
+        System.out.println("Transactions By User");
+        System.out.println("-".repeat(100));
+        int userId = getUserInt("Enter user id: ");
+        System.out.println();
+
+        var transactions = transactionDao.getTransactionByUser(userId);
+
+        System.out.print("Transactions for User ID: ");
+        System.out.print(userId);
+        System.out.println(" transaction_id | transaction_date | amount | notes ");
+        System.out.println("---------------------------------------------------");
+
+        for (Transaction transaction : transactions)
+        {
+            System.out.print(transaction.getTransactionId());
+            System.out.print(" | ");
+            System.out.print(transaction.getTransactionDate());
+            System.out.print(" | ");
+            System.out.print(transaction.getAmount());
+            System.out.print(" | ");
+            System.out.print(transaction.getNotes());
+            System.out.println();
+
+        }
+
+        waitForUser();
+    }
+
+    private void transactionsByMonth()
+    {
+
+        System.out.println();
+        System.out.println("Transactions By Month");
+        System.out.println("-".repeat(100));
+        String month = getUserString("Enter month (Format: MM) : ");
+        System.out.println();
+
+        var transactions = transactionDao.getTransactionByMonth(month);
+
+        System.out.print("Transactions for Month: ");
+        System.out.print(month);
+        System.out.println(" user_id | transaction_date | amount | notes ");
+        System.out.println("---------------------------------------------------");
+
+        for (Transaction transaction : transactions)
+        {
+            System.out.print(transaction.getUserId());
+            System.out.print(" | ");
+            System.out.print(transaction.getTransactionDate());
+            System.out.print(" | ");
+            System.out.print(transaction.getAmount());
+            System.out.print(" | ");
+            System.out.print(transaction.getNotes());
+            System.out.println();
+
+        }
+
+        waitForUser();
+    }
+
+    private void transactionsByYear()
+    {
+
+        System.out.println();
+        System.out.println("Transactions By Year");
+        System.out.println("-".repeat(100));
+        int year = getUserInt("Enter year (Format: YYYY) : ");
+        System.out.println();
+
+        var transactions = transactionDao.getTransactionByYear(year);
+
+        System.out.print("Transactions for Year: ");
+        System.out.print(year);
+        System.out.println(" user_id | transaction_date | amount | notes ");
+        System.out.println("---------------------------------------------------");
+
+        for (Transaction transaction : transactions)
+        {
+            System.out.print(transaction.getUserId());
+            System.out.print(" | ");
+            System.out.print(transaction.getTransactionDate());
+            System.out.print(" | ");
+            System.out.print(transaction.getAmount());
+            System.out.print(" | ");
+            System.out.print(transaction.getNotes());
+            System.out.println();
+
+        }
+
+        waitForUser();
+    }
+
+    private void transactionsBySubCategory()
+    {
+
+        System.out.println();
+        System.out.println("Transactions By Sub Category");
+        System.out.println("-".repeat(100));
+        int subCategoryId = getUserInt("Enter sub category id: ");
+        System.out.println();
+
+        var transactions = transactionDao.getTransactionBySubCategory(subCategoryId);
+
+        System.out.print("Transactions for Sub Category ID: ");
+        System.out.print(subCategoryId);
+        System.out.println(" user_id | transaction_date | amount | notes ");
+        System.out.println("---------------------------------------------------");
+
+        for (Transaction transaction : transactions)
+        {
+            System.out.print(transaction.getUserId());
+            System.out.print(" | ");
+            System.out.print(transaction.getTransactionDate());
+            System.out.print(" | ");
+            System.out.print(transaction.getAmount());
+            System.out.print(" | ");
+            System.out.print(transaction.getNotes());
+            System.out.println();
+
+        }
+
+        waitForUser();
+    }
+
+    private void transactionsByCategory()
+    {
+
+        System.out.println();
+        System.out.println("Transactions By Category");
+        System.out.println("-".repeat(100));
+        int categoryId = getUserInt("Enter category id: ");
+        System.out.println();
+
+        var transactions = transactionDao.getTransactionByCategory(categoryId);
+
+        System.out.print("Transactions for Category ID: ");
+        System.out.print(categoryId);
+        System.out.println(" user_id | transaction_date | sub_category_id | amount | notes ");
+        System.out.println("---------------------------------------------------");
+
+        for (Transaction transaction : transactions)
+        {
+            System.out.print(transaction.getUserId());
+            System.out.print(" | ");
+            System.out.print(transaction.getTransactionDate());
+            System.out.print(" | ");
+            System.out.print(transaction.getSubCategoryId());
+            System.out.print(" | ");
+            System.out.print(transaction.getAmount());
+            System.out.print(" | ");
+            System.out.print(transaction.getNotes());
+            System.out.println();
+
+        }
+
+        waitForUser();
+    }
+
+    // </editor-fold>
+
+    // <editor-fold desc="Add User">
+
+
+    // </editor-fold>
+
+    // <editor-fold desc="Helpers">
+
+    private void waitForUser()
+    {
+        System.out.println();
+        System.out.println("Press ENTER to continue...");
+        userInput.nextLine();
+    }
+
+    private String getUserString(String message)
+    {
+        System.out.print(message);
+        return userInput.nextLine();
+    }
+
+    private int getUserInt(String message)
+    {
+        return Integer.parseInt(getUserString(message));
+    }
+
+    private double getUserDouble(String message)
+    {
+        return Double.parseDouble(getUserString(message));
+    }
+
+    // </editor-fold>
 }
