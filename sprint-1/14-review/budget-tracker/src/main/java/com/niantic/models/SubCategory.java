ce4ee48cc3e086e990fa887a26e2b2d929c13680
Chin Antalan <152952438+chinsomnia@users.noreply.github.com>
2024-08-10 11:09:26 -0700

    Created SubCategory.java. Added getters and setters

--- sprint-1/14-review/budget-tracker/src/main/java/com/niantic/models/SubCategory.java
+++ sprint-1/14-review/budget-tracker/src/main/java/com/niantic/models/SubCategory.java
@@ -1,15 +1,47 @@
 package com.niantic.models;
 
-public class Category {
+public class SubCategory {
     // Declare private variables
-    
-
+    private int subCategoryId;
+    private String subCategoryName;
+    private String description;
 
     // Parameterless
-
+    public SubCategory()
+    {
+    }
 
     // Parameterized
-
+    public SubCategory(int subCategoryId, String subCategoryName, String description)
+    {
+        this.subCategoryId = subCategoryId;
+        this.subCategoryName = subCategoryName;
+        this.description = description;
+    }
 
     // Getters and Setters
+
+    public int getSubCategoryId() {
+        return subCategoryId;
+    }
+
+    public void setSubCategoryId(int subCategoryId) {
+        this.subCategoryId = subCategoryId;
+    }
+
+    public String getSubCategoryName() {
+        return subCategoryName;
+    }
+
+    public void setSubCategoryName(String subCategoryName) {
+        this.subCategoryName = subCategoryName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
