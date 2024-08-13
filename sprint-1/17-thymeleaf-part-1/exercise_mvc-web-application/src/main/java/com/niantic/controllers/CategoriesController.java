af71a82bbef7f76419b57c403536508e3dd46720
Chin Antalan <152952438+chinsomnia@users.noreply.github.com>
2024-08-13 15:17:58 -0700

    Add getCategoryByName() and getAllCategories() functions to controllers

--- sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/CategoriesController.java
+++ sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/CategoriesController.java
@@ -1,10 +1,45 @@
 package com.niantic.controllers;
 
+import com.niantic.models.Category;
 import com.niantic.services.CategoryDao;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 @Controller
 public class CategoriesController
 {
-    CategoryDao categoryDao = new CategoryDao();
+    private CategoryDao categoryDao = new CategoryDao();
+
+    @GetMapping("/categories")
+    public String getAllCategories(Model model, @RequestParam(required = false) String category)
+    {
+        ArrayList<Category> categories;
+
+        if(category == null)
+        {
+            categories = categoryDao.getAllCategories();
+        }
+        else
+        {
+            categories = categoryDao.getCategoryByName(category);
+        }
+
+        StringBuilder builder = new StringBuilder();
+
+        model.addAttribute("categories", categories);
+        return "categories/index";
+    }
+
+    @GetMapping("/actors/{id}")
+    public String details(Model model, @PathVariable int id)
+    {
+        var category = categoryDao.getCategoryById(id);
+        model.addAttribute("category", category);
+        return "categories/details";
+    }
 }
