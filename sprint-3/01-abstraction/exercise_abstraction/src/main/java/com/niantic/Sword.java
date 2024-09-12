373167bb925acdae88829844110e7c5d56d5d8ab
Chin <152952438+chinsomnia@users.noreply.github.com>
2024-09-11 21:49:11 -0700

    Bug fix to Sword percentCharged

--- sprint-3/01-abstraction/exercise_abstraction/src/main/java/com/niantic/Sword.java
+++ sprint-3/01-abstraction/exercise_abstraction/src/main/java/com/niantic/Sword.java
@@ -13,11 +13,14 @@ public class Sword extends Weapon
         int damage = getDamage();
         int currentCharge = getPercentCharged();
 
-        //Percent charged cannot exceed 100%
         if(currentCharge < 100)
         {
-            //Each attack adds 10% to the percentCharged
-            setPercentCharged(currentCharge + 10);
+            int updatedCharge = currentCharge + 10;
+            if(updatedCharge > 100)
+            {
+                updatedCharge = 100;
+            }
+            setPercentCharged(updatedCharge);
         }
 
         return damage;
@@ -28,7 +31,6 @@ public class Sword extends Weapon
     {
         int currentCharge = getPercentCharged();
 
-        //100% delivers a 4x blow to the other character and reduces the charge to 0
         if(currentCharge == 100)
         {
             int damage = getDamage();
@@ -36,17 +38,14 @@ public class Sword extends Weapon
 
             return damage * 4;
         }
-        //50% - 90% delivers a double blow to the other character and reduces the charge by 50%
         else if (currentCharge > 50)
         {
             int damage = getDamage();
-            int updatedCharge = currentCharge - (int) (currentCharge * .5);
-            setPercentCharged(updatedCharge);
+            currentCharge -= 50;
+            setPercentCharged(currentCharge);
 
             return damage * 2;
         }
-        //a power attack can only be used if the sword has a charge
-        //< 50% just performs a regular attack
         else if (currentCharge > 0)
         {
             return getDamage();
