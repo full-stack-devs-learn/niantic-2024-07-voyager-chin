47f834cb9bc6ea0c9619b45cedfb54be962df5d8
Chin <152952438+chinsomnia@users.noreply.github.com>
2024-09-11 21:50:29 -0700

    Create Dagger class from Weapon abstract

--- sprint-3/01-abstraction/exercise_abstraction/src/main/java/com/niantic/Dagger.java
+++ sprint-3/01-abstraction/exercise_abstraction/src/main/java/com/niantic/Dagger.java
@@ -1,5 +1,52 @@
 package com.niantic;
 
-public class Dagger
+public class Dagger extends Weapon
 {
+    private int daggerCount;
+
+    public Dagger(String name, int damage, int daggerCount)
+    {
+        super(name, damage);
+        this.daggerCount = daggerCount;
+    }
+
+    public int getDaggerCount() { return daggerCount; }
+
+    @Override
+    public int attack()
+    {
+        if (daggerCount > 0)
+        {
+            int percentCharge = getPercentCharged();
+            percentCharge += 20;
+            setPercentCharged(percentCharge);
+            return getDamage();
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    @Override
+    public int powerAttack()
+    {
+        int percentCharge = getPercentCharged();
+        int powerDamage = getDamage() * 3;
+
+        if (percentCharge == 100)
+        {
+            if(daggerCount > 0)
+            {
+                daggerCount -= 1;
+                return powerDamage;
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public int getRange() { return 10; }
+
+    public void addDagger() { daggerCount++; }
 }
