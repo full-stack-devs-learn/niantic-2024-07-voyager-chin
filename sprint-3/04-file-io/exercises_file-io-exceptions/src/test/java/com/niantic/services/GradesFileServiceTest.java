c5d7c0c224ff7af6989f5e1f6660ab30184b07c8
Chin <152952438+chinsomnia@users.noreply.github.com>
2024-09-16 23:30:18 -0700

    Created tests for GradesFileService getFileNames() and getAssignments()

---
+++ sprint-3/04-file-io/exercises_file-io-exceptions/src/test/java/com/niantic/services/GradesFileServiceTest.java
@@ -0,0 +1,43 @@
+package com.niantic.services;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import java.io.File;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GradesFileServiceTest
+{
+    GradesFileService service;
+
+    @BeforeEach
+    public void setup()
+    {
+        service = new GradesFileService();
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFileNames_shouldReturn_arrayOfFileNames()
+    {
+        String[] expectedArray = {"student_1_anna_williams.csv"
+                                , "student_2_david_jones.csv"
+                                , "student_3_laura_brown.csv"
+                                , "student_4_eric_walker.csv"
+                                , "student_5_joshua_martin.csv"};
+
+        String[] actualArray = service.getFileNames();
+
+        assertArrayEquals(expectedArray, actualArray, "Because it should return all file names inside directory");
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAssignments_shouldReturn_studentAssignments()
+    {
+        int expectedAssignmentCount = 15;
+        int actualAssignmentCount = service.getAssignments("student_5_joshua_martin.csv").size();
+
+        assertEquals(expectedAssignmentCount, actualAssignmentCount, "Because it should return all assignments in the file");
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllAssignments() {
+    }
+}
