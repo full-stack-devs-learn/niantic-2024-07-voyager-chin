56452eba22c6eb64bbb77165e316c13384758294
Chin <152952438+chinsomnia@users.noreply.github.com>
2024-09-19 23:49:34 -0700

    Updated ProductsController to return `ResponseEntity<?>`

--- sprint-3/06-rest-api/exercise_rest-api/src/main/java/com/niantic/controllers/apis/ProductsController.java
+++ sprint-3/06-rest-api/exercise_rest-api/src/main/java/com/niantic/controllers/apis/ProductsController.java
@@ -1,47 +1,141 @@
 package com.niantic.controllers.apis;
 
 import com.niantic.models.Product;
-import com.niantic.services.MySqlProductDao;
+import com.niantic.services.CategoryDao;
 import com.niantic.services.ProductDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.niantic.models.HttpError;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/products")
 public class ProductsController
 {
-    private ProductDao productDao = new MySqlProductDao();
+    private ProductDao productDao;
+    private CategoryDao categoryDao;
 
-    @GetMapping("/api/products")
-    public List<Product> getAllProducts(@RequestParam(defaultValue = "0") int catId)
+    @Autowired
+    public ProductsController(ProductDao productDao, CategoryDao categoryDao)
     {
-        if(catId > 0)
+        this.productDao = productDao;
+        this.categoryDao = categoryDao;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<?> getProducts(@RequestParam(required = false) Integer catId)
+    {
+        try
         {
-            return productDao.getProductsByCategory(catId);
+            List<Product> products = new ArrayList<>();
+
+            if(catId == null)
+            {
+                products = productDao.getProducts();
+            }
+            else
+            {
+                var category = categoryDao.getCategory(catId);
+                if(category == null)
+                {
+                    var error = new HttpError(HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND.toString(), "Category " + catId + " is invalid");
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+                }
+                products = productDao.getProductsByCategory(catId);
+            }
+
+            return ResponseEntity.ok(products);
         }
-        else
+        catch (Exception e)
         {
-            return productDao.getProducts();
+            var error = new HttpError(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), "Oops something went wrong");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
         }
     }
 
-    @GetMapping("/api/products/{id}")
-    public Product getProduct(@PathVariable int id) { return productDao.getProduct(id); }
+    @GetMapping("{id}")
+    public ResponseEntity<?> getProduct(@PathVariable int id)
+    {
+        try
+        {
+            var product = productDao.getProduct(id);
 
-    @PostMapping("/api/products")
+            if(product == null)
+            {
+                var error = new HttpError(HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND.toString(), "Product " + id + " is invalid");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+            }
+            return ResponseEntity.ok(product);
+        }
+        catch (Exception e)
+        {
+            var error = new HttpError(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), "Oops something went wrong");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+        }
+    }
+
+    @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
-    public Product addProduct(@RequestBody Product product) { return productDao.addProduct(product); }
-
-    @PutMapping("/api/products/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updateProduct(@PathVariable int id, @RequestBody Product product)
+    public ResponseEntity<?> addProduct(@RequestBody Product product)
     {
-        productDao.updateProduct(id, product);
+        try
+        {
+            productDao.addProduct(product);
+            return ResponseEntity.ok(product);
+        }
+        catch (Exception e)
+        {
+            var error = new HttpError(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), "Oops something went wrong");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+        }
     }
 
-    @DeleteMapping("/api/products/{id}")
+    @PutMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteProduct(@PathVariable int id) { productDao.deleteProduct(id); }
+    public ResponseEntity<?> updateProduct(@PathVariable int id, @RequestBody Product product)
+    {
+        try
+        {
+            var existingProduct = productDao.getProduct(id);
+            if(existingProduct == null)
+            {
+                var error = new HttpError(HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND.toString(), "Product " + id + " is invalid");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+            }
+            productDao.updateProduct(id, product);
+            return ResponseEntity.noContent().build();
+        }
+        catch (Exception e)
+        {
+            var error = new HttpError(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), "Oops something went wrong");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);        }
+    }
 
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<?> deleteProduct(@PathVariable int id)
+    {
+        try
+        {
+            var product = productDao.getProduct(id);
+
+            if(product == null)
+            {
+                var error = new  HttpError(HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND.toString(), "Product " + id + " is invalid");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+            }
+
+            productDao.deleteProduct(id);
+            return ResponseEntity.noContent().build();
+        }
+        catch (Exception e)
+        {
+            var error = new HttpError(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), "Oops something went wrong");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+        }
+    }
 }
