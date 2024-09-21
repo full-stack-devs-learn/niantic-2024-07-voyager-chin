392ae9d22ee2852f0c1329a8d3181c6e68d95c4b
Chin <152952438+chinsomnia@users.noreply.github.com>
2024-09-20 22:43:29 -0700

    Created beans controller

---
+++ sprint-3/06-rest-api/exercise_rest-api/src/main/java/com/niantic/controllers/apis/BeansController.java
@@ -0,0 +1,23 @@
+package com.niantic.controllers.apis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/beans")
+public class BeansController
+{
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @GetMapping
+    public ResponseEntity<?> getBeans()
+    {
+        var beans = applicationContext.getBeanDefinitionNames();
+        return ResponseEntity.ok(beans);
+    }
+}
