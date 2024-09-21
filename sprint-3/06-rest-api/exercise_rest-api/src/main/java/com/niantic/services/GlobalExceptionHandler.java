1854c5968dd8f3adaa37fb786ede39297a7006d1
Chin <152952438+chinsomnia@users.noreply.github.com>
2024-09-20 22:43:08 -0700

    Created GlobalExceptionHandler

---
+++ sprint-3/06-rest-api/exercise_rest-api/src/main/java/com/niantic/services/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.niantic.services;
+
+import com.niantic.models.HttpError;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler
+{
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGlobalException(Exception ex, WebRequest request)
+    {
+        // checks if 404 or 500
+        if(ex instanceof ErrorResponse && ((ErrorResponse)ex).getStatusCode() == HttpStatus.NOT_FOUND)
+        {
+            // 404 errors
+            HttpError body = new HttpError(404, HttpStatus.NOT_FOUND.toString(), "The requested resource was not found.");
+            return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+        }
+        else
+        {
+            // other errors
+            HttpError body = new HttpError(500, HttpStatus.INTERNAL_SERVER_ERROR.toString(), "Something went wrong");
+            return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
