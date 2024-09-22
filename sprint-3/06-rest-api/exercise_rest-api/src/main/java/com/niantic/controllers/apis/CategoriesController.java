f3bfcfe770e0f94cf6f57c0a5e8f5c1d26559348
Chin <152952438+chinsomnia@users.noreply.github.com>
2024-09-22 09:35:58 -0700

    Added CrossOrigin to the controllers

--- sprint-3/06-rest-api/exercise_rest-api/src/main/java/com/niantic/controllers/apis/CategoriesController.java
+++ sprint-3/06-rest-api/exercise_rest-api/src/main/java/com/niantic/controllers/apis/CategoriesController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
+@CrossOrigin
 public class CategoriesController
 {
     private CategoryDao categoryDao = new MySqlCategoryDao();
--- sprint-3/06-rest-api/exercise_rest-api/src/main/java/com/niantic/controllers/apis/ProductsController.java
+++ sprint-3/06-rest-api/exercise_rest-api/src/main/java/com/niantic/controllers/apis/ProductsController.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/products")
+@CrossOrigin
 public class ProductsController
 {
     private ProductDao productDao;
