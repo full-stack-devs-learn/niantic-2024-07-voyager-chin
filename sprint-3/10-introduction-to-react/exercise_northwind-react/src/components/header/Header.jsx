a0dd130b20d8a38cf6f37cdf2932ec0c1d49b4cf
Chin <152952438+chinsomnia@users.noreply.github.com>
2024-09-25 00:12:23 -0700

    Created Header component

---
+++ sprint-3/10-introduction-to-react/exercise_northwind-react/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import  './Header.css';
+
+export default function Header() {
+    return (
+        <>
+            <div id="navigation">
+                <img src="images/logo.png" alt="Northwind Logo" />
+                <h1>Northwind Traders</h1>
+            </div>
+        </>
+    )
+}
