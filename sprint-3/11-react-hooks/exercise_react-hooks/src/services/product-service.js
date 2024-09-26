4e611f85d6013c161529493a4c52f25861c2dc63
Chin <152952438+chinsomnia@users.noreply.github.com>
2024-09-26 00:26:26 -0700

    Added ProductService with axios

---
+++ sprint-3/11-react-hooks/exercise_react-hooks/src/services/product-service.js
@@ -0,0 +1,53 @@
+import axios from 'axios'
+
+class ProductService
+{
+    baseUrl = 'http://localhost:8080/products'
+
+    getProducts(categoryId = null) 
+    {
+        if (categoryId == null)
+        {
+            return axios.get(this.baseUrl)
+                .then(response => {
+                    return response.data;
+                })
+        }
+        else
+        {
+            return axios.get(`${this.baseUrl}?catId=${categoryId}`)
+                .then(response => {
+                    return response.json();
+                })
+        }
+    }
+
+    getProductByID(productId)
+    {
+        return axios.get(`${this.baseUrl}/${categoryId}`)
+            .then(response => {
+                return response.data;
+            })
+    }
+
+    addProduct(product)
+    {
+        return axios.post(this.baseUrl, product)
+            .then(response => {
+                return response.data;
+            }) 
+    }
+
+    updateProduct(productId, product)
+    {
+        return axios.put(`${this.baseUrl}/${productId}`, product);
+    }
+
+    deleteProduct(productId)
+    {
+        return axios.delete(`${this.baseUrl}/${productId}`);
+    }
+}
+
+const productService = new ProductService();
+export default productService;
