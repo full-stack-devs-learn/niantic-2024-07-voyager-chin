902b72faeff56db80dbc9af734e88778c2760ddd
Chin <152952438+chinsomnia@users.noreply.github.com>
2024-09-28 01:06:53 -0700

    Fix checkbox input for AddProduct

--- sprint-3/11-react-hooks/exercise_react-hooks/src/components/products/product-add/ProductAdd.jsx
+++ sprint-3/11-react-hooks/exercise_react-hooks/src/components/products/product-add/ProductAdd.jsx
@@ -10,7 +10,7 @@ export default function ProductAdd({ onCancel, onProductAdded }) {
     const [unitsInStock, setUnitsInStock] = useState('');
     const [unitsOnOrder, setUnitsOnOrder] = useState('');
     const [reorderLevel, setReorderLevel] = useState('');
-    const [discontinued, setDiscontinued] = useState('');
+    const [discontinued, setDiscontinued] = useState('true');
 
     async function addProductHandler(event) {
         event.preventDefault()
@@ -32,60 +32,66 @@ export default function ProductAdd({ onCancel, onProductAdded }) {
         onProductAdded()
     }
 
+    const checkboxChange = (event) => {
+        setDiscontinued(event.target.checked);
+    }
+
     return (
-        <div className="container">
-            <h2>Add New Product</h2>
-            <form onSubmit={addProductHandler}>
-                <div className="row">
-                    <label htmlFor="product-name" className="form-label mt-4">Product Name:</label>
-                    <input type="text" className="form-control" name="product-name" id="product-name"
-                        onChange={(e) => setProductName(e.target.value)} />
-                </div>
-                <div className="row">
-                    <label htmlFor="supplier-id" className="form-label mt-4">Supplier Id:</label>
-                    <input type="text" className="form-control" name="supplier-Id" id="supplier-id"
-                        onChange={(e) => setSupplierId(e.target.value)} />
-                </div>
-                <div className="row">
-                    <label htmlFor="category-id" className="form-label mt-4">Category Id:</label>
-                    <input type="text" className="form-control" name="category-id" id="category-id"
-                        onChange={(e) => setCategoryId(e.target.value)} />
-                </div>
-                <div className="row">
-                    <label htmlFor="quantity-per-unit" className="form-label mt-4">Quantity per Unit:</label>
-                    <input type="text" className="form-control" name="quantity-per-unit" id="quantity-per-unit"
-                        onChange={(e) => setQuantityPerUnit(e.target.value)} />
-                </div>
-                <div className="row">
-                    <label htmlFor="unit-price" className="form-label mt-4">Unit Price:</label>
-                    <input type="text" className="form-control" name="unit-price" id="unit-price"
-                        onChange={(e) => setUnitPrice(e.target.value)} />
-                </div>
-                <div className="row">
-                    <label htmlFor="units-in-stock" className="form-label mt-4">Units in Stock:</label>
-                    <input type="text" className="form-control" name="units-in-stock" id="units-in-stock"
-                        onChange={(e) => setUnitsInStock(e.target.value)} />
-                </div>
-                <div className="row">
-                    <label htmlFor="units-on-order" className="form-label mt-4">Units on Order:</label>
-                    <input type="text" className="form-control" name="units-on-order" id="units-on-order"
-                        onChange={(e) => setUnitsOnOrder(e.target.value)} />
-                </div>
-                <div className="row">
-                    <label htmlFor="reorder-level" className="form-label mt-4">Reorder Level:</label>
-                    <input type="text" className="form-control" name="reorder-level" id="reorder-level"
-                        onChange={(e) => setReorderLevel(e.target.value)} />
-                </div>
-                <div className="row">
-                    <div className="form-check form-switch">
-                        <label htmlFor="discontinued" className="form-check-label">Discontinued:</label>
-                        <input type="checkbox" className="form-control form-check-input" name="discontinued" id="discontinued"
-                            onChange={(e) => setDiscontinued(e.target.value)} />
+        <div className="container mt-5 mb-5">
+            <h3>Add New Product</h3>
+            <div className="container">
+                <form onSubmit={addProductHandler}>
+                    <div className="row">
+                        <label htmlFor="product-name" className="form-label mt-4">Product Name:</label>
+                        <input type="text" className="form-control" name="product-name" id="product-name"
+                            onChange={(e) => setProductName(e.target.value)} />
                     </div>
-                </div>
-                <button className="btn btn-danger mr-3" type="submit">Add Product</button>
-                <button className="btn btn-dark" type="cancel" onClick={onCancel}>Cancel</button>
-            </form>
+                    <div className="row">
+                        <label htmlFor="supplier-id" className="form-label mt-4">Supplier Id:</label>
+                        <input type="text" className="form-control" name="supplier-Id" id="supplier-id"
+                            onChange={(e) => setSupplierId(e.target.value)} />
+                    </div>
+                    <div className="row">
+                        <label htmlFor="category-id" className="form-label mt-4">Category Id:</label>
+                        <input type="text" className="form-control" name="category-id" id="category-id"
+                            onChange={(e) => setCategoryId(e.target.value)} />
+                    </div>
+                    <div className="row">
+                        <label htmlFor="quantity-per-unit" className="form-label mt-4">Quantity per Unit:</label>
+                        <input type="text" className="form-control" name="quantity-per-unit" id="quantity-per-unit"
+                            onChange={(e) => setQuantityPerUnit(e.target.value)} />
+                    </div>
+                    <div className="row">
+                        <label htmlFor="unit-price" className="form-label mt-4">Unit Price:</label>
+                        <input type="text" className="form-control" name="unit-price" id="unit-price"
+                            onChange={(e) => setUnitPrice(e.target.value)} />
+                    </div>
+                    <div className="row">
+                        <label htmlFor="units-in-stock" className="form-label mt-4">Units in Stock:</label>
+                        <input type="text" className="form-control" name="units-in-stock" id="units-in-stock"
+                            onChange={(e) => setUnitsInStock(e.target.value)} />
+                    </div>
+                    <div className="row">
+                        <label htmlFor="units-on-order" className="form-label mt-4">Units on Order:</label>
+                        <input type="text" className="form-control" name="units-on-order" id="units-on-order"
+                            onChange={(e) => setUnitsOnOrder(e.target.value)} />
+                    </div>
+                    <div className="row">
+                        <label htmlFor="reorder-level" className="form-label mt-4">Reorder Level:</label>
+                        <input type="text" className="form-control" name="reorder-level" id="reorder-level"
+                            onChange={(e) => setReorderLevel(e.target.value)} />
+                    </div>
+                    <div className="row">
+                            <label htmlFor="discontinued" className="form-check-label mt-4">{discontinued ? "Discontinued" : "Product discontinued?"}</label>
+                            <input type="checkbox" checked={discontinued} onChange={checkboxChange}
+                                className="form-control form-check-input" name="discontinued" id="discontinued"/>
+                                </div>
+                    <span className="container">
+                        <button className="btn btn-danger mt-3" type="submit">Add Product</button>
+                    </span>
+                    <button className="btn btn-dark mt-3" type="cancel" onClick={onCancel}>Cancel</button>
+                </form>
+            </div>
         </div>
     )
 }
