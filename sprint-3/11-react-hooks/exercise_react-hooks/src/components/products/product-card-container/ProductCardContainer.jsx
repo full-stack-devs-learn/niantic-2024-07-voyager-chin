dece92fd18b0ab84cbc61a02ef40f3a74928df89
Chin <152952438+chinsomnia@users.noreply.github.com>
2024-09-28 01:06:13 -0700

    Added deleteProduct function to ProductCard

--- sprint-3/11-react-hooks/exercise_react-hooks/src/components/products/product-card-container/ProductCardContainer.jsx
+++ sprint-3/11-react-hooks/exercise_react-hooks/src/components/products/product-card-container/ProductCardContainer.jsx
@@ -1,23 +1,20 @@
 import { useEffect, useState } from "react";
 import ProductCard from "../product-card/ProductCard";
-import productService from "../../../services/product-service";
 import './ProductCardContainer.css'
+import productService from "../../../services/product-service";
 
-export default function ProductCardContainer()
-{
-    let [selectedProduct, setSelectedProduct] = useState("None Selected");
-    let [selectedProductId, setSelectedProductId] = useState(0);
-    let [products, setProducts] = useState([]);
+export default function ProductCardContainer() {
+    const [selectedProduct, setSelectedProduct] = useState("None Selected");
+    const [selectedProductId, setSelectedProductId] = useState(0);
+    const [products, setProducts] = useState([]);
 
     useEffect(() => {
-
         productService.getProducts().then(data => {
             setProducts(data);
         })
     }, []); // empty array means run only on first render
 
-    const productSelected = (name) =>
-    {
+    const productSelected = (name) => {
         setSelectedProduct(name);
 
         const productId = products.filter(p => p.productName === name)[0].productId;
@@ -31,17 +28,31 @@ export default function ProductCardContainer()
     }
 
     return (
-        <main className="container mt-4 products-container" id="products-container">
-            {
-                products.map((product) => (
-                    <ProductCard key={product.productId}
-                    productId={product.productId}
-                    product={product.productName}
-                    unitPrice={product.unitPrice}
-                    ></ProductCard>
-                ))
-            }
+        <main className="container mt-4 mb-5 products-container" id="products-container">
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Product Name</th>
+                        <th>Category Id</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        products.map((product) => (
+                            <ProductCard key={product.productId}
+                                productId={product.productId}
+                                product={product.productName}
+                                categoryId={product.categoryId}
+                                unitPrice={product.unitPrice}
+                                onProductSelected={productSelected}
+                                onProductDeleted={productDeleted}
+                            ></ProductCard>
+                        ))
+                    }
+                </tbody>
+            </table>
         </main>
     )
-
 }
--- sprint-3/11-react-hooks/exercise_react-hooks/src/components/products/product-card/ProductCard.jsx
+++ sprint-3/11-react-hooks/exercise_react-hooks/src/components/products/product-card/ProductCard.jsx
@@ -1,18 +1,28 @@
+import { useState } from "react";
 import productService from "../../../services/product-service";
 import './ProductCard.css';
+import { XCircle, XCircleFill } from 'react-bootstrap-icons'
 
-export default function ProductCard({ id, productId, product, unitPrice }) {
-    const imageUrl = 'https://cdn-icons-png.flaticon.com/128/3731/3731085.png';
+export default function ProductCard({ id, productId, product, categoryId, unitPrice, onProductSelected, onProductDeleted }) {
+    const productClicked = () => {
+        onProductSelected(product)
+    }
+
+    async function deleteProduct(event) {
+        event.stopPropagation();
+
+        await productService.deleteProduct(productId);
+
+        onProductDeleted(productId);
+    }
 
     return (
-        <table className="table table-hover">
-            <tbody>
-                <tr className="table-light">
-                    <td className="id">{productId}</td>
-                    <td className="product">{product}</td>
-                    <td className="price">${unitPrice}</td>
-                </tr>
-            </tbody>
-        </table>
+        <tr className="table-light">
+            <td className="id">{productId}</td>
+            <td className="product">{product}</td>
+            <td className="category">{categoryId}</td>
+            <td className="price">$ {unitPrice}</td>
+            <td><XCircleFill onClick={deleteProduct} color="red" /></td>
+        </tr>
     )
 }
