f305053c06b907a452a8188fe695b56239fcaf0c
Chin <152952438+chinsomnia@users.noreply.github.com>
2024-10-02 01:32:48 -0700

    Added AppDispatch and RootState to categoriesList

--- sprint-3/14-react-routing/exercise_react-routing/src/components/categories/categories-list/CategoriesList.tsx
+++ sprint-3/14-react-routing/exercise_react-routing/src/components/categories/categories-list/CategoriesList.tsx
@@ -1,8 +1,38 @@
+import { Link } from "react-router-dom";
+import { Category } from "../../../models/category";
+import { useDispatch } from "react-redux";
+import { AppDispatch, RootState } from "../../../store/store";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { loadCategories } from "../../../store/features/categories-slice";
+
 export default function CategoriesList()
 {
+    const dispatch = useDispatch<AppDispatch>()
+    const { categories, loading, error } = useSelector((state: RootState) => state.categories);
+
+    useEffect(() => {
+        if(categories.length == 0)
+        {
+            dispatch(loadCategories(null))
+        }
+    }, [dispatch])
+
+    if (loading) return <p>Loading categories...</p>
+    if (error) return <p>Error: {error}</p>
+
     return (
         <>
             <h1>Categories List</h1>
+            <Link className="btn btn-success" to="/categories/add">Add</Link>
+
+            <ul>
+                {
+                    categories.map((c: Category) => (
+                        <li key={c.categoryId}><Link to={`/categories/${c.categoryId}`}>{c.categoryName}</Link></li>
+                    ))
+                }
+            </ul>
         </>
     )
 }
