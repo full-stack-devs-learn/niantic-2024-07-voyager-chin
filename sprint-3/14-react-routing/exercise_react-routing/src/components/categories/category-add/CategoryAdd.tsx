e8f27319ba27344dd4772f4abcdf5b7fa9372b96
Chin <152952438+chinsomnia@users.noreply.github.com>
2024-10-02 15:19:03 -0700

    Added loadProducts from products-slice

--- sprint-3/14-react-routing/exercise_react-routing/src/components/categories/category-add/CategoryAdd.tsx
+++ sprint-3/14-react-routing/exercise_react-routing/src/components/categories/category-add/CategoryAdd.tsx
@@ -1,8 +1,20 @@
+import { Category } from "../../../models/category";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { AppDispatch, RootState } from "../../../store/store";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { loadCategories } from "../../../store/features/categories-slice";
+
 export default function CategoryAdd()
 {
+    // const dispatch = useDispatch<AppDispatch>()
+    // const { categories, loading, error } = useSelector((state: RootState) => state.categories);
+
     return (
         <div className="container">
             <h1>Add Category</h1>
+
         </div>
     )
 }
--- sprint-3/14-react-routing/exercise_react-routing/src/components/products/products-list/ProductsList.tsx
+++ sprint-3/14-react-routing/exercise_react-routing/src/components/products/products-list/ProductsList.tsx
@@ -1,17 +1,45 @@
-import { useSearchParams } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { AppDispatch, RootState } from "../../../store/store";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { loadProducts } from "../../../store/features/products-slice";
+import { Product } from "../../../models/product";
 
 export default function ProductsList()
 {
-    const [searchParams] = useSearchParams();
+    // const [searchParams] = useSearchParams();
 
-    const minPrice = searchParams.get('minPrice');
-    const maxPrice = searchParams.get('maxPrice');
-    const name = searchParams.get('name');
-    const catId = searchParams.get('catId');
+    // const minPrice = searchParams.get('minPrice');
+    // const maxPrice = searchParams.get('maxPrice');
+    // const name = searchParams.get('name');
+    // const catId = searchParams.get('catId');
+
+    const dispatch = useDispatch<AppDispatch>()
+    const { products, loading, error } = useSelector((state: RootState) => state.products);
+
+    useEffect(() => {
+        if(products.length == 0)
+        {
+            dispatch(loadProducts(null))
+        }
+    }, [dispatch])
+
+    if(loading) return <p>Loading products...</p>
+    if(error) return <p>Error: {error}</p>
 
     return (
         <>
             <h1>Products List</h1>
+            <Link className="btn btn-success" to="/products/add">Add</Link>
+
+            <ul>
+                {
+                    products.map((p: Product) => (
+                        <li key={p.productId}><Link to={`/products/${p.productId}`}>{p.productName}</Link></li>
+                    ))
+                }
+            </ul>
         </>
     )
 }
