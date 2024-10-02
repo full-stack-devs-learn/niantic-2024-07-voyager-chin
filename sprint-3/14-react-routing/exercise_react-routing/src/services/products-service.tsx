96dbe03a6bd986841c6172aa9005efc8cb59956d
Chin <152952438+chinsomnia@users.noreply.github.com>
2024-10-02 14:08:21 -0700

    Created products service using axios

---
+++ sprint-3/14-react-routing/exercise_react-routing/src/services/products-service.tsx
@@ -0,0 +1,49 @@
+import axios from "axios";
+import { Product } from "../models/product";
+
+class ProductService
+{
+    baseUrl = `${import.meta.env.VITE_API_BASE_URL}/categories`;
+
+    async getProducts(categoryId = null): Promise<Product[]>
+    {
+        if (categoryId == null) {
+            return axios.get<Product[]>(this.baseUrl)
+                .then(response => {
+                    return response.data;
+                })
+        }
+        else {
+            return axios.get<Product[]>(`${this.baseUrl}?catId=${categoryId}`)
+                .then(response => {
+                    return response.data;
+                })
+        }
+    }
+
+    getProductByID(productId: number): Promise<Product>
+    {
+        return axios.get<Product>(`${this.baseUrl}/${productId}`)
+            .then(response => {
+                return response.data;
+            })
+    }
+
+    add(product: Product) {
+        return axios.post(this.baseUrl, product)
+            .then(response => {
+                return response.data;
+            })
+    }
+
+    update(productId: number, product: Product) {
+        return axios.put<void>(`${this.baseUrl}/${productId}`, product);
+    }
+
+    delete(productId: number) {
+        return axios.delete<void>(`${this.baseUrl}/${productId}`);
+    }
+}
+
+const productService = new ProductService();
+export default productService;
