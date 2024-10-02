a606583b2898b6e65c996a643b1f56fb732920f7
Chin <152952438+chinsomnia@users.noreply.github.com>
2024-10-01 23:29:22 -0700

    Added provider and store to root

--- sprint-3/14-react-routing/exercise_react-routing/src/main.tsx
+++ sprint-3/14-react-routing/exercise_react-routing/src/main.tsx
@@ -4,9 +4,13 @@ import 'bootstrap/dist/js/bootstrap.min.js'
 import 'bootswatch/dist/cosmo/bootstrap.min.css'
 import './index.css'
 import App from './App'
+import { Provider } from 'react-redux'
+import store from './store/store'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </StrictMode>,
 )
